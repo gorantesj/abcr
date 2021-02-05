a5c56597e402a513c65d89fd04ae18c3fb60d0fc
gorantesj <gorantesj@hotmail.com>
2021-02-05 09:11:05 -0600

    Se arreglaron detalles gráficos de la evaluación de estimación para una muestra

--- R/evaluar_estimacion.R
+++ R/evaluar_estimacion.R
@@ -8,6 +8,7 @@
 #'
 #' @examples
 evaluar_estimacion <- function(resultado, marco_muestral){
+  browser()
   cand <- resultado %>% pull(candidato) %>% unique()
   reales <- marco_muestral %>%
     summarise(across(all_of(cand),~sum(.x))) %>%
@@ -20,22 +21,24 @@ evaluar_estimacion <- function(resultado, marco_muestral){
     filter(!contenido) %>%
     mutate(referencia=if_else(valor>ic_975, ic_975, ic_025),
            diferencia=valor-referencia,
-           referencia=case_when(referencia<max(ic_975)*.5 ~ ic_975,
-                                referencia>max(ic_975)*.9 ~ ic_025))
+           referencia=case_when(referencia<max(completa$ic_975)*.5 ~ ic_975,
+                                referencia>max(completa$ic_975)*.9 ~ ic_025,
+                                T ~ est_puntual))
   res <- completa %>%
     ggplot()+
     geom_linerange(aes(x=forcats::fct_reorder(candidato,ic_975),
                        ymin=ic_025,
-                       ymax=ic_975), color="#f46036")+
+                       ymax=ic_975),
+                   color="#f46036")+
     geom_point(aes(x=candidato, y=valor, color=as.factor(contenido))) +
     scale_x_discrete(name="Candidatos")+
     scale_y_continuous(labels = scales::percent_format(), name = "Votación (%)") +
     scale_color_manual(values = c("TRUE"="#1b998b","FALSE"="#e71d36"),
                        name="Contiene al valor",
-                       labels=c("Sí","No"))+
+                       labels=c("TRUE"="Sí", "FALSE"="No")) +
     annotate(x=letrero$candidato,
-             y=letrero$valor,
-             label=round(letrero$diferencia,4),
+             y=letrero$referencia,
+             label=scales::percent(letrero$diferencia,accuracy = 0.01),
              vjust=-0.5,
              nudge_y=.1,
              geom="text") +
